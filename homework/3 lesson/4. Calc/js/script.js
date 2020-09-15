360e04a28ddc041110a08ac15b24db5d9883c624
EllisiumMax <sailorjerry777@gmail.com>
2020-09-15 12:59:30 +0300

    implemented function readOperand to check validity

--- homework/3 lesson/4. Calc/js/script.js
+++ homework/3 lesson/4. Calc/js/script.js
@@ -3,27 +3,25 @@
 let result = 0;
 let mathOperator = prompt("Input operator (+, -, *, /, **, %)").trim();
 
+function readOperand(message) {
+  let operand = +(prompt(message)).trim();
+  while (isNaN(operand)) {
+    alert("Operand must be a digit");
+    operand = +(prompt(message)).trim();
+    if (operand === null) break;
+  }
+  return operand;
+}
+
+
 while (mathOperator !== "+" && mathOperator !== "-" && mathOperator !== "*" && mathOperator !== "/" && mathOperator !== "**" && mathOperator !== "%") {
     alert("This is not valid operator");
     mathOperator = prompt("Input operator (+, -, *, /, **, %)").trim();
     if (mathOperator === null) break;
 }
 
-let operandA = +prompt("Input first operand").trim();
-
-while (isNaN(operandA)) {
-    alert("Operand must be a digit");
-    operandA = +prompt("Input first operand").trim();
-    if (operandA === null) break;
-}
-
-let operandB = +prompt("Input second operand").trim();
-
-while (isNaN(operandB)) {
-    alert("Operand must be a digit");
-    operandB = +prompt("Input second operand").trim();
-    if (operandB === null) break;
-}
+let operandA = readOperand("Input first operand");
+let operandB = readOperand("Input second operand");
 
 switch (mathOperator) {
     case "+":
@@ -36,8 +34,8 @@ switch (mathOperator) {
         result = operandA * operandB;
         break;
     case "/":
-        result = (operandB === 0) ? "Error, can't divide by zero!" : operandA / operandB;
-        break;
+	    result = operandB === 0 ? result = "Error, can't divide by zero!" : operandA / operandB;
+        break
     case "**":
         result = operandA ** operandB;
         break;
