8fb0ef63c1ca00a9f51e99b06b995fd940add9c1
EllisiumMax <sailorjerry777@gmail.com>
2020-09-19 09:33:54 +0300

    v 0.3.1 - working

--- homework/4 lesson/5. Calc v. 2/js/script.js
+++ homework/4 lesson/5. Calc v. 2/js/script.js
@@ -5,26 +5,26 @@
 let numbers = [];
 let selectedOperator;
 
-function inputOperator () {
+function inputOperator() {
     const validOperators = ["+", "*"];
     let operator;
-    while(!validOperators.includes(operator)) {
+    while (!validOperators.includes(operator)) {
         operator = prompt("Enter operator ( + or * )");
         if (operator === null) return alert("Cancelled by user.");
         else if (!validOperators.includes(operator)) alert("Error: Enter valid operator! ( + or * )")
     };
-    return selectedOperator = operator; 
+    return selectedOperator = operator;
 }
 
-function validateOperand (value) {
+function validateOperand(value) {
     if (value === null) return;
     else if (value.trim().length > 6) return alert("Operator length must be less than 6 digits!");
     else if (!Number.isInteger(+value)) return alert("Operand must be an integer number!");
     else return numbers.push(+value);
- 
+
 }
 
-function inputOperand () { 
+function inputOperand() {
     let operand;
     while (operand !== null) {
         operand = prompt("Input operand");
@@ -32,31 +32,31 @@ function inputOperand () {
     }
 }
 
-function sumValues (value) {
+function sumValues(value) {
     let result = 0;
     let expression = "";
     for (let i = 0; i < value.length; i++) {
         result += value[i];
         expression += value[i] + " + ";
     }
-    return alert(expression.slice(0,expression.length - 2) + " = " + result);
+    return alert(expression.slice(0, expression.length - 2) + " = " + result);
 }
 
-function multiplyValues (value) {
+function multiplyValues(value) {
     let result = 1;
     let expression = "";
     for (let i = 0; i < value.length; i++) {
         result *= value[i];
         expression += value[i] + " * ";
     }
-    return alert(expression.slice(0,expression.length - 2) + " = " + result);
+    return alert(expression.slice(0, expression.length - 2) + " = " + result);
 }
 
 
 
-function calculate (operator) {
-    switch(operator) {
-        case "+": 
+function calculate(operator) {
+    switch (operator) {
+        case "+":
             sumValues(numbers);
             break;
         case "*":
@@ -64,7 +64,6 @@ function calculate (operator) {
     }
 }
 
-inputOperator ();
-inputOperand ();
-calculate (selectedOperator);
-
+inputOperator();
+inputOperand();
+calculate(selectedOperator);
