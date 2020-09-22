c65236f9571e86fc21b8f9e1efdb0b20820cec60
EllisiumMax <sailorjerry777@gmail.com>
2020-09-22 17:03:04 +0300

    started homework #6

---
+++ homework/5 lesson/6. Arrays/js/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const resultsObj = {
+  allValues: [], //(массив всех введенных значений)
+  elements: 0, //(общее количество элементов массива)
+  minInteger: 0, //(наименьшее целое число)
+  maxInteger: 0, //(наибольшее целое число)
+  arithmeticMean: 0, //(среднее арифметическое)
+  evenPositiveIntegerElements: 0, //(количество четных положительных целых чисел)
+  negativeElements: 0, //(количество отрицательных чисел)
+  fractionSum: 0, //(сумма всех дробных чисел)
+};
+
+function userInput() {
+  //запрашивает значения и сохраняет их в массив, пока пользователь не нажмет отмена.
+}
+
+function findQuantityOfValues(array) {
+  // находит кол-во значений в массиве.
+}
+
+function findMaxMinInteger(array) {
+  // находит максимальное и минимальное значение в массиве.
+}
+
+function findAverageInteger(array) {
+  // находит среднее арифметичесское всех целых чисел.
+}
+
+function findEvenPositiveIntegers(array) {
+  // находит общее количество четных положительных целых чисел.
+}
+
+function findNegativeNumbers(array) {
+  // находит количество отрицательных чисел.
+}
+
+function findSumOfFractionalNumbers(array) {
+  //  находит сумму всех дробных чисел.
+}
+
+console.log(resultsObj);
