4a7ef0ec46d8c74a16755fc24486273ddbeac6fb
EllisiumMax <sailorjerry777@gmail.com>
2020-09-29 14:53:12 +0300

    Optimized

--- homework/7 lesson/8. Closure/js/script.js
+++ homework/7 lesson/8. Closure/js/script.js
@@ -4,20 +4,21 @@ function closureDivide (dividerValue = 1) {
   let result = 0;
   return function (value) {
     result = value / dividerValue;
-    dividerValue = Math.ceil(result);
+    if (result >= 1) dividerValue = Math.round(result);
+    else dividerValue = Math.ceil(result);
     return result;
   }
 }
 
-let divide = closureDivide(2); // делитель по умолчанию передается в аргумент этой функции
+let divide = closureDivide(6); // делитель по умолчанию передается в аргумент этой функции
 
-// console.log(divide(2));
-// console.log(divide(1));
-// console.log(divide(7));
-// console.log(divide(70));
-// console.log(divide(2));
-
+console.log(divide(2));
 console.log(divide(1));
 console.log(divide(7));
 console.log(divide(70));
 console.log(divide(2));
+
+// console.log(divide(1));
+// console.log(divide(7));
+// console.log(divide(70));
+// console.log(divide(2));
