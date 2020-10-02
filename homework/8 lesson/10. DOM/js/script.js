6cf051810970ae8107dfa42c393f9287d3ff1eaf
EllisiumMax <sailorjerry777@gmail.com>
2020-10-02 12:55:25 +0300

    v. 0.1 working

--- homework/8 lesson/10. DOM/js/script.js
+++ homework/8 lesson/10. DOM/js/script.js
@@ -2,24 +2,47 @@
 
 const buttonAddLink = document.querySelector("#addLink");
 const buttonClearAll = document.querySelector("#clearAll");
-
+const linkList = document.querySelector("#main");
 
 function addLink () {
-    let webAdress = prompt('Please enter a web adress', 'https://www.google.com');
-    if (webAdress === null || webAdress == '') return;
-    let element = document.createElement("li");
-    const linksList = document.querySelector("#links");
-    element.className = "user-link";
-    element.innerHTML = `<a href="${webAdress}">${webAdress}</a>`;
-    linksList.append(element);
+    let id = 0;
+    return function () {
+        const webAdress = prompt('Please enter a web adress', 'https://www.google.com');
+        if (webAdress === null || webAdress == '') return;
+        const element = document.createElement("li");
+        const linksList = document.querySelector("#links");
+        element.className = "user-link";
+        element.id = "link" + ++id;
+        element.innerHTML = `<a href="${webAdress}">${webAdress}</a>`;
+        linksList.append(element);
+        const button = document.createElement("button");
+        button.className = "button-delete";
+        button.id = "delete" + id;
+        button.innerHTML = "Удалить";
+        element.append(button);
+    };
 }
 
+
+
 function clearLinks () {
-    let emptyNode = document.createElement("ol");
+    const emptyNode = document.createElement("ol");
     const linksList = document.querySelector("#links");
     emptyNode.id = "links";
     linksList.replaceWith(emptyNode);
 }
 
-buttonAddLink.onclick = addLink;
+function deleteLink (event) {
+    let eventID = event.target.id.slice(6);
+    const link = document.querySelector(`#link${eventID}`);
+    console.log(eventID);
+    if (link) link.remove();
+    else return;
+    
+}
+
+buttonAddLink.onclick = addLink();
 buttonClearAll.onclick = clearLinks;
+linkList.addEventListener("click", deleteLink);
+
+
