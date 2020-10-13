8b09c8cd6170bc4596d7a4dbe9173f8ed490e7f8
EllisiumMax <sailorjerry777@gmail.com>
2020-10-13 23:10:27 +0300

    Basic functionality done, working

--- homework/10 lesson/main.js
+++ homework/10 lesson/main.js
@@ -8,7 +8,6 @@ const listRejected = document.getElementById("rejected");
 const templateListName = document.getElementById("template-list-name");
 const templateListItem = document.getElementById("template-list-item");
 
-
 function addToDo() {
   const value = inputField.value;
 
@@ -26,35 +25,51 @@ function addToDo() {
       .replace("{{btn-text}}", "Delete");
     inputField.value = "";
     const checkbox = document.querySelector(".checkbox");
-  listToDo.addEventListener("change", moveToDone);
-    
+    listToDo.addEventListener("change", moveToDone);
   }
-  
 }
 
 function moveToDone(event) {
-
-
-if (listDone.querySelector(".done") === null) {
-  listDone.innerHTML = templateListName.innerHTML.replace(
-    "{{listName}}",
-    "Done"
-  );
-}
+  if (listDone.querySelector(".done") === null) {
+    listDone.innerHTML = templateListName.innerHTML.replace(
+      "{{listName}}",
+      "Done"
+    );
+  }
 
   listDone.innerHTML += templateListItem.innerHTML
     .replace("{{class}}", "done")
     .replace("{{text}}", event.target.nextSibling.textContent)
     .replace('<button class="{{reject-restore}}">{{btn-text}}</button>', "");
-    event.target.parentElement.remove();
-    event.target.remove();
-    if (listToDo.querySelector(".to-do") === null) {
-      listToDo.innerHTML = "";
-    }
-    const checkbox = listDone.querySelectorAll(".checkbox");
-    checkbox.forEach((element) => {
-      element.setAttribute("checked","checked");
-    });
+  event.target.parentElement.remove();
+  event.target.remove();
+  if (listToDo.querySelector(".to-do") === null) {
+    listToDo.innerHTML = "";
+  }
+  const checkbox = listDone.querySelectorAll(".checkbox");
+  checkbox.forEach((element) => {
+    element.setAttribute("checked", "checked");
+  });
+  listDone.addEventListener("change", restoreTask);
+}
+
+function restoreTask (event) {
+  if (listToDo.querySelector(".to-do") === null) {
+    listToDo.innerHTML = templateListName.innerHTML.replace(
+      "{{listName}}",
+      "To Do"
+    );
+  }
+  listToDo.innerHTML += templateListItem.innerHTML
+  .replace("{{text}}", event.target.nextSibling.textContent)
+  .replace("{{class}}", "to-do")
+  .replace("{{reject-restore}}", "btn-delete")
+  .replace("{{btn-text}}", "Delete");
+  event.target.parentElement.remove();
+  event.target.remove();
+  if (listDone.querySelector(".done") === null) {
+    listDone.innerHTML = "";
+  }
 }
 
 submitButton.addEventListener("click", addToDo);
