456769a0b9fd8d363e1b645bfaac4e0478b296c3
EllisiumMax <sailorjerry777@gmail.com>
2020-10-21 01:59:12 +0300

    Rebuild, and design change

--- homework/12 lesson/main.js
+++ homework/12 lesson/main.js
@@ -1,30 +1,42 @@
 "use strict";
 
-const SUBMIT = document.querySelector("#submit");
+const SUBMIT = document.querySelector(".submit");
 const COMMENTS_AREA = document.querySelector("#user_comments");
 const USER_NAME = document.querySelector("#input_name");
 const USER_COMMENT = document.querySelector("#input_comment");
+const TEMPLATE_COMMENT = document.querySelector("#template_comment");
 
-SUBMIT.addEventListener("click", (e) => {
-  e.preventDefault();
-  const singleComment = document.createElement("div");
-  const userName = document.createElement("p");
-  const userComment = document.createElement("p");
-  const deleteComment = document.createElement("button");
-  const editComment = document.createElement("button");
+function insertComment() {
+  if (!USER_NAME.value.trim() || !USER_COMMENT.value.trim()) return;
+  const currentDate = new Date();
+  const dateOptions = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  };
+  const timeStamp = currentDate.toLocaleString("ru", dateOptions);
 
+  COMMENTS_AREA.innerHTML += TEMPLATE_COMMENT.innerHTML
+    .replace("{{name}}", USER_NAME.value)
+    .replace("{{comment}}", USER_COMMENT.value)
+    .replace("{{date}}", timeStamp);
+}
 
-  singleComment.className = "comment";
-  userName.textContent = USER_NAME.value;
-  userComment.textContent = USER_COMMENT.value;
-  deleteComment.innerHTML= "DELETE";
-  editComment.innerHTML = "EDIT";
+document.addEventListener("click", (e) => {
+  const tar = e.target.className;
+  switch (tar) {
+    case "submit":
+      insertComment();
+      break;
+    default:
+      return;
+  }
+});
 
-
-  COMMENTS_AREA.append(singleComment);
-  singleComment.append(userName);
-  singleComment.append(userComment);
-  singleComment.append(deleteComment);
-  singleComment.append(editComment);
-
-} );
+document.addEventListener("focusin", (e) => {
+  if (e.target.tagName !== "INPUT") return;
+  e.target.value = "";
+});
