ff60024557d88abeeff2411bf7bc09c31d79a0ac
EllisiumMax <sailorjerry777@gmail.com>
2020-10-29 17:37:44 +0200

    final

--- homework/14_lesson/main.js
+++ homework/14_lesson/main.js
@@ -34,8 +34,8 @@ function insertComment() {
         animateSubmit(true);
         ++commentsData.counter;
         const $newComment = $template
-            .replace("{{name}}", formatName($("#input_name").val()))
-            .replace("{{comment}}", formatComment($("#input_comment").val()))
+            .replace("{{name}}", $("#input_name").val())
+            .replace("{{comment}}", $("#input_comment").val())
             .replace("{{date}}", moment().format("D/MM/YYYY, HH:mm:ss"))
             .replace("{{index}}", "comment_0" + commentsData.counter);
 
@@ -44,12 +44,13 @@ function insertComment() {
             behavior: "smooth",
         });
         commentsData["comment_0" + commentsData.counter] = {
-            name: formatName($("#input_name").val()),
-            comment: formatComment($("#input_comment").val()),
+            name: $("#input_name").val(),
+            comment: $("#input_comment").val(),
             date: moment().format("D/MM/YYYY, HH:mm:ss"),
             edited: false,
             index: "comment_0" + commentsData.counter,
         };
+        $("#input_comment").val("");
     }
 }
 
@@ -79,6 +80,8 @@ function editComment(e) {
                     ".user_comment").text()) {
 
                 if (validateInput()) {
+                    const commentValue = formatComment($(
+                        ".modal-comment").val());
                     $(".modal-ok").off();
                     const dateNow = moment().format(
                         "D/MM/YYYY, HH:mm:ss");
@@ -90,7 +93,7 @@ function editComment(e) {
                     $(e.target)
                         .parent()
                         .children(".user_comment")
-                        .text($(".modal-comment").val());
+                        .html(commentValue);
                     $(e.target)
                         .parent()
                         .children(".date")
@@ -102,10 +105,10 @@ function editComment(e) {
                     $(".modal-wrapper").slideUp(200);
                     commentsData[$commentID].name = $(".modal-name")
                         .val();
-                    commentsData[$commentID].comment = $(
-                        ".modal-comment").val();
+                    commentsData[$commentID].comment = commentValue;
                     commentsData[$commentID].date = dateNow;
                     commentsData[$commentID].edited = true;
+                    $("#input_comment").val("");
                 }
             }
         });
@@ -113,7 +116,28 @@ function editComment(e) {
 }
 
 function errorWarning() {
-    $(".modal-error").fadeIn(300).delay(1000).fadeOut(500);
+    $(".modal-error").fadeIn(300).delay(2500).fadeOut(500);
+}
+
+function formatComment(commentText) {
+    const findLink =
+        /\b(?<protocol>https?:\/\/)(?<domain>\w{1,63}\.[^\s/.:,]{1,63}(\.[^\s/.:,]{2,63})?(\.[^\s/.:,]{2,63})?)(?<port>\:\d{1,5})?(?<path>\/[\w\/]+(\.(\w{1,8}))?)?\b/mig;
+    const findHtml =
+        /<!?\/?\w+.+>.*<\/?\w+>|<!?\/?\w+.+>?/mig;
+    const findBoldText = /(\*\*)([^*][{\p{P}\p{L}\p{N}]+?)(\*\*)/gmiu;
+    const findStrikeTroughText = /(--)([^*][{\p{P}\p{L}\p{N}]+?)(--)/gmiu;
+    const findItalicText = /(__)([^*][{\p{P}\p{L}\p{N}]+?)(__)/gmiu;
+    const formatedComment = commentText.replace(findHtml, "")
+        .replace(findLink, '<a href="$&" class="user-link">$&</a>')
+        .replace(findBoldText, "<strong>$2</strong>")
+        .replace(findStrikeTroughText, "<s>$2</s>")
+        .replace(findItalicText, "<i>$2</i>");
+    return formatedComment;
+}
+
+function formatName(nameText) {
+    const checkName = /^([A-Z][a-z]+\s?){1,3}$/;
+    if (nameText.match(checkName)) return true;
 }
 
 function validateInput() {
@@ -134,6 +158,13 @@ function validateInput() {
         errorWarning();
         $errorWindow.text("Error: Please enter your name");
         return;
+    } else if (!formatName($name)) {
+        animateSubmit(false);
+        errorWarning();
+        $errorWindow.text(
+            "Error: Name should consist of maximum three words, each word should start with capittal letter"
+        );
+        return;
     } else if ($name.length < 3) {
         animateSubmit(false);
         errorWarning();
@@ -142,6 +173,7 @@ function validateInput() {
         animateSubmit(false);
         errorWarning();
         $errorWindow.text("Error: Name length must be less than 50 symbols");
+
     } else if (!$comment) {
         animateSubmit(false);
         errorWarning();
@@ -155,31 +187,37 @@ function validateInput() {
         errorWarning();
         $errorWindow.text(
             "Error: Comment maximum length must be less than 300 symbols");
-    } else return true;
+    } else {
+        $("#input_comment").val(formatComment($comment));
+        if ($("#input_comment").val() == "") {
+            animateSubmit(false);
+            errorWarning();
+            $errorWindow.text(
+                "Error: HTML tags are not allowed! Input only text!");
+            return false;
+        } else return true;
+    }
 }
 
 $(".submit").on("click", insertComment);
 $("#input_area").on("keydown", (e) => {
+    const keyEnter = 13;
+    const keyEscape = 27;
     switch (e.which) {
-    case 13:
+        //По замечанию по поводу deprecated which из прошлого ДЗ.
+        // We've decided to not mark this API as deprecated in the end as there's no direct native replacement. See https://github.com/jquery/jquery/issues/4755#issuecomment-664501730
+    case keyEnter:
         insertComment();
         break;
-    case 27:
+    case keyEscape:
         clearInput(e);
         break;
     }
 });
 
 $(document)
-    .on("click", clearInput)
     .on("click", deleteComment)
     .on("click", editComment);
-
-const observer = new MutationObserver(() => {
-    const jsonObj = JSON.stringify(commentsData);
-    localStorage.setItem("comments", jsonObj);
-});
-
 
 function build() {
     if (localStorage.getItem("comments")) {
@@ -188,7 +226,6 @@ function build() {
     getValues(commentsData);
 
     function getValues(object) {
-
         const $template = $("#template_comment").html();
         for (let key in object) {
             if (typeof object[key] === "object") {
@@ -198,7 +235,9 @@ function build() {
                 const date = object[key].date;
                 const edited = object[key].edited;
                 const index = object[key].index;
-                ++commentsData.counter;
+
+
+
                 const $newComment = $template
                     .replace("{{name}}", name)
                     .replace("{{comment}}", comment)
@@ -216,34 +255,43 @@ function build() {
     }
 }
 
+$("#input_comment, .modal-comment").on('select', (e) => {
+    const selectedText = window.getSelection().toString();
+    let comment;
+    if ($(".modal-wrapper").attr("display") == "none") {
+        comment = $("#input_comment");
+    } else comment = $(".modal-comment");
+
+    if (selectedText && selectedText !== "") {
+        $(".bold").off().on("click", () => {
+            let editedText = comment.val().replace(
+                selectedText, `**${selectedText}**`);
+            comment.val(editedText);
+            editedText = "";
+        })
+        $(".strike").off().on("click", () => {
+            let editedText = comment.val().replace(
+                selectedText, `--${selectedText}--`);
+            comment.val(editedText);
+            editedText = "";
+        })
+        $(".italic").off().on("click", () => {
+            let editedText = comment.val().replace(
+                selectedText, `__${selectedText}__`);
+            comment.val(editedText);
+            editedText = "";
+        })
+    }
+});
+
+const observer = new MutationObserver(() => {
+    const jsonObj = JSON.stringify(commentsData);
+    localStorage.setItem("comments", jsonObj);
+});
+
 observer.observe(document, {
     subtree: true,
     childList: true,
 });
+
 window.onload = build();
-
-function formatComment(commentText) {
-    const findLink =
-        /\b(?<protocol>https?:\/\/)(?<domain>\w{1,63}\.[^\s/.:,]{1,63}(\.[^\s/.:,]{2,63})?(\.[^\s/.:,]{2,63})?)(?<port>\:\d{1,5})?(?<path>\/[\w\/]+(\.(\w{1,8}))?)?\b/mig;
-    const findHtml =
-        /(<!?\s*[a-z]+\s*.*[0-9]?>).*\n?(<\/\s*[a-z]+\s*[0-9]?>)/mig;
-    const findBoldText = /(\*\*)([\p{L}\p{N}]+)(\*\*)/gmiu;
-    const findStrikeTroughText = /(--)([\p{L}\p{N}]+)(--)/gmiu;
-    const findItalicText = /(__)([\p{L}\p{N}]+)(__)/gmiu;
-    const formatedComment = commentText.replace(findHtml, "*HTML NOT PERMITED")
-        .replace(findLink, '<a href="$&" class="user-link">$&</a>')
-        .replace(findBoldText, "<strong>$2</strong>")
-        .replace(findStrikeTroughText, "<s>$2</s>")
-        .replace(findItalicText, "<i>$2</i>");
-    return formatedComment;
-}
-
-function formatName(nameText) {
-    const checkName = /^([A-Z][a-z]+\s?){1,3}$/;
-    if (nameText.match(checkName)) return nameText;
-    else {
-        alert("wrong name!");
-        nameText = "Enter valid name such as Max Maximov";
-        return nameText;
-}
-}
