abff3093088182d9f46dffe18e52e4d76fcebaf3
EllisiumMax <sailorjerry777@gmail.com>
2020-12-26 13:32:43 +0200

    improved info window visual

--- project_tornado/js/info-window.js
+++ project_tornado/js/info-window.js
@@ -1,19 +1,22 @@
 "use strict";
 
 const infoWindowUI = {
+    windowWrapper: document.createElement("div"),
     window: document.createElement("div"),
     text: document.createElement("p"),
     show(message, ms = 800) {
-        this.window.style.opacity = 1;
+        this.windowWrapper.style.opacity = 1;
+        this.windowWrapper.className = "info-window-wrapper";
         this.window.className = "info-window";
-        document.body.append(this.window);
+        document.body.append(this.windowWrapper);
+        this.windowWrapper.append(this.window);
         this.window.append(this.text);
         this.text.textContent = message;
         this.hide(ms);
 
     },
     hide(ms) {
-        setTimeout( () => this.window.style.opacity = 0, ms);
-        setTimeout( () => this.window.remove(), ms + 200);
+        setTimeout( () => this.windowWrapper.style.opacity = 0, ms);
+        setTimeout( () => this.windowWrapper.remove(), ms + 200);
     }
 }
